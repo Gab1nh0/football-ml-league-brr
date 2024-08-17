1b34c8037a30a4a6c6f2c9a477afe24271f30938
Gab1nh0 <gabriel507xs@gmail.com>
2024-08-17 16:30:33 -0500

    se agregan listado de equipos

---
+++ src/app/teams/page.tsx
@@ -0,0 +1,55 @@
+"use client";
+import { useState, useEffect } from 'react';
+import { db } from '../Config/firebase';
+import { collection, getDocs, QueryDocumentSnapshot, DocumentData } from 'firebase/firestore';
+import styles from './teams.module.css';
+import Link from 'next/link';
+
+interface Team {
+    id: string;
+    Squad: string;
+    color: string;
+    img: string;
+}
+
+export default function Teams() {
+    const [data, setData] = useState<Team[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const querySnapshot = await getDocs(collection(db, 'teams'));
+                const documents: Team[] = querySnapshot.docs.map((doc: QueryDocumentSnapshot<DocumentData>) => ({
+                    id: doc.id,
+                    ...doc.data() as Team,
+                }));
+                setData(documents);
+            } catch (error) {
+                console.error("Error fetching teams: ", error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return (
+        <div className={styles.section}>
+            <h1>Equipos</h1>
+            <br />
+            {data.map((item) => (
+                <div key={item.id}>
+                    <div className={styles.teamcontainer} style={{ backgroundColor: item.color }}>
+                        <div className={styles.teamimgcontainer}>
+                            <img src={item.img} alt={item.Squad} />
+                        </div>
+                        
+                        <Link href={`/roster/${item.Squad}`}>
+                            <h1>{item.Squad}</h1>
+                        </Link>
+                    </div>
+                    <br />
+                </div>
+            ))}
+        </div>
+    );
+}
